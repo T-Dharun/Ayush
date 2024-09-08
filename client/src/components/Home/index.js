ef1daf98bd596e4a370162f933c323f9c52a9416
satheesh1022005 <satheesh1022005@gmail.com>
2024-09-09 01:47:29 +0530

    try

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,3 +1,2 @@
-import CertificateForm from "./CertificateForm";
 import ProgressBar from "./ProgressBar";
 export { CertificateForm, ProgressBar };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useState,useEffect } from "react"
-import {CertificateForm } from "../components/Home"
 import Bot from "../pages/bot"
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
