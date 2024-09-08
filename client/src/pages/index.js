e03a536024f586a9e9eae00e5325a084cc68fbb5
satheesh1022005 <satheesh1022005@gmail.com>
2024-09-09 01:20:41 +0530

    altered

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,7 @@ import GovernWorkspace from "./GovernWorkspace";
 import Home from "./Home";
 import Status from "./Status";
 import ForgotPassword from "./ForgotPassword";
-import Courses from "./courses";
+import Courses from "./Courses";
 import Bot from "./bot";
 import CertificateGenerator from "./CertificateGenerator";
 export {
